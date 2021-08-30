414ce779129f2a5bee91a0378ff30d3ce72ef638
Sabelius <sbebby92@virgilio.it>
2021-08-30 15:51:05 +0200

    aggiornamento

--- js/myscript.js
+++ js/myscript.js
@@ -2,23 +2,23 @@ let couponList = ["Discount01", "Discount02", "Discount03"];
 let nomePanino = document.getElementById("nomePanino");
 let prezzoBasePanino;
 let ingredienteAggiunto;
-console.log(nomePanino);
+let prezzoFinale;
 
-let burgerButton =document.getElementById("calculatePrice");
-console.log(calculatePrice);
+
+let burgerButton = document.getElementById("calculatePrice");
 
 
 burgerButton.addEventListener("click",
-    function(){
-        if(nomePanino.value.lenght === 0){
+    function () {
+        if (nomePanino.value.lenght === 0) {
             alert("Inserisci il nome del panino!!!")
-        }else{
-            prezzoBasePanino = 14;
-            ingredienteAggiunto = document.getElementsByClassName("ingredients");
+        } else {
+           prezzoBasePanino = 50;
+           ingredienteAggiunto = document.getElementsByClassName("ingredients");
         }
-        for(i = 0; i < ingredienteAggiunto.lenght; i++){
-            let ingredienteAggiunto = ingredienti[i];
-            if( ingredienteAggiunto.checked == true){
+        for (i = 0; i < ingredienteAggiunto.lenght; i++) {
+           let ingredienteScelto = ingredientiAggiunto[i];
+            if (ingredienteScelto.checked == true) {
                 prezzoBasePanino += 5;
             }
         }
@@ -26,13 +26,17 @@ burgerButton.addEventListener("click",
         let coupon = document.getElementById("insertCoupon");
 
 
-        if(coupon.value.lenght > 0){
-            if(couponList.includes(coupon.value)){
-                prezzoBasePanino = prezzoBasePanino * 1.4;
-            } else{
+        if (coupon.value.lenght > 0) {
+            if (couponList.includes(coupon.value)) {
+                prezzoBasePanino = prezzoBasePanino * 0.5;
+            } else {
                 alert("Inserisci un coupon valido.");
             }
         }
-        let totale = document.getElementById("somma").innerHTML = prezzoBasePanino + "&euro;";
-    })
+
+        prezzoFinale = document.getElementById("somma").innerHTML = prezzoBasePanino + "&euro;";
+
+    });
+
+
 
